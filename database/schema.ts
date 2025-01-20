100d559fdaff7a08d0e4707df63d7b0b807464be
watashi407 <hyperteddy70@gmail.com>
2025-01-21 01:45:55 +0800

    Setup database using drizzle

---
+++ database/schema.ts
@@ -0,0 +1,40 @@
+import {
+  varchar,
+  date,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+import { University } from "lucide-react";
+import { string } from "zod";
+
+export const STATUS_ENUM = pgEnum("status", [
+  "PENDING",
+  "APPROVED",
+  "REJECTED",
+]);
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
+  "BORROWED",
+  "RETURNED",
+]);
+
+export const users = pgTable("users", {
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+  password: text("password").notNull(),
+  universityCard: text("university_card").notNull(),
+  status: STATUS_ENUM("status").default("PENDING"),
+  role: ROLE_ENUM("role").default("USER"),
+  lastActivityDate: date("last_activity_date").defaultNow(),
+  createdAt: timestamp("create_at", { withTimezone: true }).defaultNow(),
+});
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
