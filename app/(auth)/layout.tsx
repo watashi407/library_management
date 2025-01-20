5d0239097844ca035ef0e51fcf687b8e0c0d9cd5
watashi407 <hyperteddy70@gmail.com>
2025-01-20 14:14:07 +0800

    Setup Auth Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import Image from "next/image";
+
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-2">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+          <div>{children}</div>
+        </div>
+      </section>
+
+      {children}
+    </main>
+  );
+}
+
+export default layout;
