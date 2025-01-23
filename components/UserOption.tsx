b1006137016baebf8cf932a6d17086af5602861a
watashi407 <hyperteddy70@gmail.com>
2025-01-23 15:15:01 +0800

    Update logout transition

--- components/UserOption.tsx
+++ components/UserOption.tsx
@@ -11,33 +11,46 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { getInitials } from "@/lib/utils";
-import { Button } from "./ui/button";
 import { signOutAuth } from "@/lib/actions/auth";
+import { useTransition } from "react";
 import Link from "next/link";
 
 const UserOption = ({ session }: { session: Session }) => {
-  console.log(`Sesion value`, session);
+  const [isPending, startTransition] = useTransition();
   return (
-    <section>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          {" "}
-          <Avatar>
-            <AvatarFallback className="font-bold bg-primary">
-              {getInitials(session?.user?.name as string)}
-            </AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="font-bold bg-primary">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={signOutAuth}> Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </section>
+    <>
+      {isPending ? (
+        <>
+          <div>loading...</div>
+        </>
+      ) : (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              {" "}
+              <Avatar>
+                <AvatarFallback className="font-bold bg-primary">
+                  {getInitials(session?.user?.name as string)}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="font-bold bg-primary">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href="/profile">Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={() => startTransition(async () => await signOutAuth())}
+              >
+                {" "}
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
+      )}
+    </>
   );
 };
 
