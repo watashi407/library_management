46836f557fd682850332fba076e6614db019617d
watashi407 <hyperteddy70@gmail.com>
2025-01-24 23:01:28 +0800

    Implement Sidebar

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { adminSideBarLinks } from "@/constant";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import path from "path";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="admin-sidebar">
       <div>
@@ -14,7 +22,39 @@ const Sidebar = () => {
           />
           <h1>Watashi Libro</h1>
         </div>
-        <div className="mt-10"></div>
+        <div className="mt-10 flex flex-col gap-5">
+          {adminSideBarLinks.map((link) => {
+            const isSelected =
+              (link.route !== "/admin" &&
+                pathname.includes(link.route) &&
+                link.route.length > 1) ||
+              pathname === link.route;
+
+            return (
+              <div key={link.route} className="flex items-center gap-5">
+                <Link
+                  href={link.route}
+                  className={cn(
+                    "link",
+                    isSelected && "bg-primary-admin shadow-sm"
+                  )}
+                >
+                  <div className="relative size-5">
+                    <Image
+                      src={link.img}
+                      alt="icons"
+                      fill
+                      className={`${isSelected ? "brightness-0 invert" : ""} object-contain`}
+                    />
+                  </div>
+                  <p className={cn(isSelected ? "text-white" : "text-dark")}>
+                    {link.text}
+                  </p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
