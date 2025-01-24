5abd15086c8cd97ca8d4f2977a5b8dd8ba40c75b
watashi407 <hyperteddy70@gmail.com>
2025-01-25 02:18:49 +0800

    All books layout setup

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -5,11 +5,16 @@ import React from "react";
 const page = () => {
   return (
     <section className="w-full bg-white rounded-2xl p-7">
-      <div className="flex flex-wrap item-center justify-between gap-2">
+      <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className="text-xl font-semibold">All Books</h2>
         <Button className="bg-primary-admin" asChild>
-          <Link href={`/admin/books/new`}>+ Create New Book</Link>
+          <Link href={`/admin/books/new`} className="text-white">
+            + Create New Book
+          </Link>
         </Button>
+      </div>
+      <div className="mt-7 w-full overflow-hidden">
+        <p>Table</p>
       </div>
     </section>
   );
