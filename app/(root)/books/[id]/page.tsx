b40a87a7b7240879f1dd0fc7abb260d0053dfc86
watashi407 <hyperteddy70@gmail.com>
2025-01-26 23:39:56 +0800

    update book route

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = ({ params }: { params: string }) => {
-  return <div>{params}</div>;
+  return <div>{params} Its temporary</div>;
 };
 
 export default page;
