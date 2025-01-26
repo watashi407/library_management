fc4c7099a0f40ae7de7fd35517131f4510476b6e
watashi407 <hyperteddy70@gmail.com>
2025-01-26 23:15:37 +0800

    change image render

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,7 +1,8 @@
 import { cn } from "@/lib/utils";
 import React from "react";
-import Image from "next/image";
 import BookCoverSvg from "@/components/BookCoverSvg";
+import { IKImage } from "imagekitio-next";
+import config from "@/lib/config/config";
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
@@ -39,11 +40,13 @@ function BookCover({
         className="absolute z-10"
         style={{ left: "12%", width: "87.5%", height: "88%" }}
       >
-        <Image
-          src={coverImage}
+        <IKImage
+          path={coverImage}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
           alt="Book Cover"
           fill
           className="rounded-sm object-fill"
+          loading="lazy"
         />
       </div>
     </div>
