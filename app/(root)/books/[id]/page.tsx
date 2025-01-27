e4f9aa661637db1dec82f2ccdda9cdb941aac8b0
watashi407 <hyperteddy70@gmail.com>
2025-01-27 17:02:10 +0800

    Add video component

--- app/(root)/books/[id]/page.tsx
+++ app/(root)/books/[id]/page.tsx
@@ -5,6 +5,7 @@ import { db } from "@/database/drizzle";
 import { redirect } from "next/navigation";
 import BookOverview from "@/components/BookOverview";
 import { checkUserSession } from "@/hooks/user_session";
+import BookVideo from "@/components/BookVideo";
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -25,6 +26,23 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         bookDetails={bookDetails}
         userId={session?.user?.id as string}
       />
+      <div className="book-details">
+        <div className="flex[1.5]">
+          <section className="flex flex-col gap-7">
+            <h3>video</h3>
+            <BookVideo videoUrl={bookDetails.videoUrl} />
+          </section>
+          <section className="mt-10 flex flex-col gap-7">
+            <h3>Summary</h3>
+            <div className="space-y-5 text-xl text-light-100">
+              {bookDetails.summary.split("\n").map((line, i) => (
+                <p key={i}>{line}</p>
+              ))}
+            </div>
+          </section>
+        </div>
+        {/** Similar Book Component */}
+      </div>
     </>
   );
 };
